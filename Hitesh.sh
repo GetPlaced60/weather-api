1cd0c8ad3225a7bda2a5f845d619559b628195a1
GetPlaced60 <getplaced60@gmail.com>
2024-05-23 14:39:03 +0530

    Learn shell-scripting

--- Hitesh.sh
+++ Hitesh.sh
@@ -1,2 +1,3 @@
 # This is my shell-scripting world
 echo "Hello welcome here to learn Shell-scripting"
+echo "A shell script is a computer program designed to be run by a Unix shell, a command-line interpreter"
