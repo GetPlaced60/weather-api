996fa95a6f8192fd61996a8aa5a31731af60d9cd
GetPlaced60 <getplaced60@gmail.com>
2024-05-24 12:44:58 +0530

    Added

--- Hitesh.sh
+++ Hitesh.sh
@@ -1,3 +1,5 @@
 # This is my shell-scripting world
 echo "Hello welcome here to learn Shell-scripting"
 echo "A shell script is a computer program designed to be run by a Unix shell, a command-line interpreter"
+echo "Hi this is me"
+echo "This is getplaced"
